1aff0c35c070ff3b25df005dec5922a7a96d93da
Vitor Moreira <vitormoreira5091@gmail.com>
2025-02-18 00:39:43 -0300

    Caca-Bugs

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 //CAÇA BUGS - CONDICIONAIS
-
 let iniciar = prompt("Deseja iniciar o programa? s/n")
 
 if (iniciar == "s") {
-    alert("Programa encerrado!")
-} else {
     alert("Programa iniciado!")
-    let opcao = prompt("Escolha uma ação a ser feita -> 1 - Mensagem motivacional; 2 - Mensagem desmotivacional; 3 - nada")
-    switch (iniciar) {
-        case 1:
-            alert("Você NÃO VAI conseguir, desista!")
-        case 2:
+    let opcao = prompt("Escolha uma ação a ser feita: \n1 - Mensagem motivacional; \n2 - Mensagem desmotivacional; \n3 - nada")
+    switch (opcao) {
+        case "1":
             alert("Continue tentando, você VAI conseguir!")
-        case 3:
-            alert("Opção inválida selecione um número de 1 a 3.")
+            break;
+        case "2":
+            alert("Você NÃO VAI conseguir, desista!")
+            break;
+        case "3":
+            alert("Nada feito. Até mais!")
+            break;
         default:
-            alert("...")
+            alert("Opção inválida selecione um número de 1 a 3.")
+            break;
     }
-}
+} else {
+    alert("Programa encerrado!")
+    }
+
+
